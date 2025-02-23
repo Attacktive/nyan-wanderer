b2b89432d8460fdf9b4e3248e3d13e3d2e979f28
Attacktive <tribute2pantera@gmail.com>
2025-02-23 14:14:24 +0900

    refactor: make bash code more robust

--- uninstall.sh
+++ uninstall.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-rm -fr $HOME/.local/share/plasma/plasmoids/xyz.attacktive.nyan-wanderer
-rm -fr $HOME/.local/share/metainfo/xyz.attacktive.nyan-wanderer.appdata.xml
+rm -fr "$HOME/.local/share/plasma/plasmoids/xyz.attacktive.nyan-wanderer"
+rm -fr "$HOME/.local/share/metainfo/xyz.attacktive.nyan-wanderer.appdata.xml"
